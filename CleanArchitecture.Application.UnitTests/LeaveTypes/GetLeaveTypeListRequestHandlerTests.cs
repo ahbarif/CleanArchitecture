f42d37e65dd84ae04137692c19aa8854b62916c7
Arif Abdullah <ahb.arif@gmail.com>
2025-02-13 23:39:01 +0600

    Unit test

---
+++ CleanArchitecture.Application.UnitTests/LeaveTypes/GetLeaveTypeListRequestHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.DTOs.LeaveType;
+using CleanArchitecture.Application.Features.LeaveTypes.Handlers.Queries;
+using CleanArchitecture.Application.Features.LeaveTypes.Requests.Queries;
+using CleanArchitecture.Application.Profiles;
+using CleanArchitecture.Application.UnitTests.Mock;
+using Moq;
+using Shouldly;
+
+namespace CleanArchitecture.Application.UnitTests.LeaveTypes
+{
+    public class GetLeaveTypeListRequestHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ILeaveTypeRepository> _mockRepo;
+        public GetLeaveTypeListRequestHandlerTests()
+        {
+            _mockRepo = MockLeaveTypeRepository.GetLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfiles>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListTest()
+        {
+            var handler = new GetLeaveTypeListRequestHandler(_mockRepo.Object, _mapper);
+
+            var result = await handler.Handle(new GetLeaveTypeListRequest(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<LeaveTypeDto>>();
+
+            result.Count.ShouldBe(2);
+        }
+    }
+}
---
+++ CleanArchitecture.Application.UnitTests/Mock/MockLeaveTypeRepository.cs
@@ -0,0 +1,39 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain.Entities;
+using Moq;
+
+namespace CleanArchitecture.Application.UnitTests.Mock
+{
+    public static class MockLeaveTypeRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>
+            {
+                new LeaveType 
+                { 
+                    Id = 1, 
+                    Name = "Vacation",
+                    DefaultDays = 10
+                },
+                 new LeaveType
+                {
+                    Id = 2,
+                    Name = "Sick",
+                    DefaultDays = 12
+                }
+            };
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+
+            mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(leaveTypes);
+            mockRepo.Setup(r => r.AddAsync(It.IsAny<LeaveType>())).ReturnsAsync((LeaveType leaveType) =>
+            {
+                leaveType.Id = leaveTypes.Count + 1;
+                leaveTypes.Add(leaveType);
+                return leaveType;
+            });
+
+            return mockRepo;
+        }
+    }
+}
